90ea8235ef8a0a3bb9fceddac0b8ee7cb30159a4
Leonard <leonard@mimisiku.dev>
2022-09-03 23:57:33 +0200

    remove useless comments

--- src/pages/home-part.ts
+++ src/pages/home-part.ts
@@ -4,19 +4,16 @@ import { createNoise2D } from 'simplex-noise';
 import hsl from 'hsl-to-hex';
 import debounce from 'debounce';
 
-// return a random number within a range
 function random(min: number, max: number) {
   return Math.random() * (max - min) + min;
 }
 
-// map a number from 1 range to another
 function map(n: number, start1:number, end1:number, start2:number, end2:number) {
   return ((n - start1) / (end1 - start1)) * (end2 - start2) + start2;
 }
 
 const noise2D = createNoise2D();
 
-// ColorPalette class
 class ColorPalette {
   hue: number | null = null;
   complimentaryHue1: number | null = null;
@@ -34,30 +31,24 @@ class ColorPalette {
   }
 
   setColors() {
-    // pick a random hue somewhere between 220 and 360
     this.hue = ~~random(220, 360);
     this.complimentaryHue1 = this.hue + 30;
     this.complimentaryHue2 = this.hue + 60;
-    // define a fixed saturation and lightness
     this.saturation = 95;
     this.lightness = 50;
 
-    // define a base color
     this.baseColor = hsl(this.hue, this.saturation, this.lightness);
-    // define a complimentary color, 30 degress away from the base
     this.complimentaryColor1 = hsl(
       this.complimentaryHue1,
       this.saturation,
       this.lightness
     );
-    // define a second complimentary color, 60 degrees away from the base
     this.complimentaryColor2 = hsl(
       this.complimentaryHue2,
       this.saturation,
       this.lightness
     );
 
-    // store the color choices in an array so that a random one can be picked later
     this.colorChoices = [
       this.baseColor,
       this.complimentaryColor1,
@@ -86,8 +77,6 @@ class ColorPalette {
     );
   }
 }
-
-// Orb class
 class Orb {
   bounds: { x: { min: number; max: number; }; y: { min: number; max: number; }; };
   x: number;
@@ -99,34 +88,23 @@ class Orb {
   yOff: number;
   inc: number;
   graphics: Graphics;
-  // Pixi takes hex colors as hexidecimal literals (0x rather than a string with '#')
+
   constructor(fill = 0x000000) {
-    // bounds = the area an orb is "allowed" to move within
     this.bounds = this.setBounds();
-    // initialise the orb's { x, y } values to a random point within it's bounds
     this.x = random(this.bounds['x'].min, this.bounds['x'].max);
     this.y = random(this.bounds['y'].min, this.bounds['y'].max);
 
-    // how large the orb is vs it's original radius (this will modulate over time)
     this.scale = 1;
-
-    // what color is the orb?
     this.fill = fill;
-
-    // the original radius of the orb, set relative to window height
     this.radius = random(window.innerHeight / 6, window.innerHeight / 3);
 
-    // starting points in "time" for the noise/self similar random values
     this.xOff = random(0, 1000);
     this.yOff = random(0, 1000);
-    // how quickly the noise/self similar random values step through time
     this.inc = 0.002;
 
-    // PIXI.Graphics is used to draw 2d primitives (in this case a circle) to the canvas
     this.graphics = new Graphics();
     this.graphics.alpha = 0.825;
 
-    // 250ms after the last window resize event, recalculate orb positions.
     window.addEventListener(
       'resize',
       debounce(() => {
@@ -146,7 +124,6 @@ class Orb {
         ? window.innerHeight
         : window.innerHeight / 1.375;
 
-    // allow each orb to move x distance away from it's x / y origin
     return {
       x: {
         min: originX - maxDist,
@@ -177,19 +154,14 @@ class Orb {
   }
 
   render() {
-    // update the PIXI.Graphics position and scale values
     this.graphics.x = this.x;
     this.graphics.y = this.y;
     this.graphics.scale.set(this.scale);
 
-    // clear anything currently drawn to graphics
     this.graphics.clear();
 
-    // tell graphics to fill any shapes drawn after this with the orb's fill color
     this.graphics.beginFill(this.fill);
-    // draw a circle at { 0, 0 } with it's size set by this.radius
     this.graphics.drawCircle(0, 0, this.radius);
-    // let graphics know we won't be filling in any more shapes
     this.graphics.endFill();
   }
 }
@@ -199,12 +171,9 @@ export function create() {
     if(!canvas) { return; }
 
     const app = new Application({
-        // render to <canvas class="background-canvas"></canvas>
         view: canvas,
-        // auto adjust size to fit the current window
         resizeTo: window,
-        // transparent background, we will be creating a gradient background later using CSS
-        transparent: true
+        backgroundAlpha: 0.3
   });
   
   // Create colour palette
