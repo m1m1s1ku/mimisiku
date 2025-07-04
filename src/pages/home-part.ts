fdb42b388933b7cecf87076c7ce08d9795e6f96a
Leonard <leonard@mimisiku.dev>
2025-07-04 04:13:47 +0200

    chore: cleanup imports

--- src/pages/home-part.ts
+++ src/pages/home-part.ts
@@ -1,4 +1,4 @@
-import { Application, Graphics, settings, utils } from 'pixi.js';
+import { Application, Graphics, settings } from 'pixi.js';
 import { KawaseBlurFilter } from '@pixi/filter-kawase-blur';
 import { createNoise2D } from 'simplex-noise';
 import hsl from 'hsl-to-hex';
